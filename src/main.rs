b6af6309176d355b8330cc569699d5b2f7be6280
Alexey Vyskubov <alexey@ocaml.nl>
2023-02-18 11:18:43 +0000

    Acquire and print system load.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use sysinfo::{CpuExt, CpuRefreshKind, RefreshKind, System, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys =
+        System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything()));
+    std::thread::sleep(std::time::Duration::from_millis(1100));
+    sys.refresh_cpu();
+    println!("{}%", sys.global_cpu_info().cpu_usage());
 }
